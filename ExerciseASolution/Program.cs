50d321d2b9c52e97aa7b712b0797f009fa824b56
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-01-24 12:59:43 +0100

    with proxy server

--- ExerciseASolution/Program.cs
+++ ExerciseASolution/Program.cs
@@ -3,12 +3,15 @@ using System.Text.Json;
 using ExerciseASolution;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
+using Startup;
 using WebSocketBoilerplate;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOptionsWithValidateOnStart<AppOptions>()
     .Bind(builder.Configuration.GetSection(nameof(AppOptions)));
+builder.Services.AddSingleton<IProxyConfig, ProxyConfig>();
+
 
 var appOptions = builder.Configuration.GetSection(nameof(AppOptions)).Get<AppOptions>();
 var redisConfig = new ConfigurationOptions
@@ -17,7 +20,7 @@ var redisConfig = new ConfigurationOptions
 };
 
 //For production deployment with gcloud avoid using comma separated connectionstring
-if (appOptions.DragonFlyConnectionString.StartsWith("rediss://") || 
+if (appOptions.DragonFlyConnectionString.StartsWith("rediss://") ||
     appOptions.DragonFlyConnectionString.StartsWith("redis://"))
 {
     redisConfig.Ssl = appOptions.DragonFlyConnectionString.StartsWith("rediss://");
@@ -39,6 +42,11 @@ var app = builder.Build();
 var opts = app.Services.GetRequiredService<IOptionsMonitor<AppOptions>>().CurrentValue;
 Console.WriteLine(JsonSerializer.Serialize(opts));
 app.Services.GetRequiredService<CustomWebSocketServer>().Start(app);
-//run on port 8080
-var url = "http://0.0.0.0:8081";
-app.Run(url);
+app.Urls.Clear();
+const int restPort = 5000;
+const int wsPort = 8181;
+var publicPort = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "8080");
+app.Urls.Add($"http://0.0.0.0:{restPort}");
+app.Services.GetRequiredService<IProxyConfig>().StartProxyServer(publicPort, restPort, wsPort);
+
+app.Run();
